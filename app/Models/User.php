1f5938cb2ffb7e988f657a8688d1d112fffabac0
williamfonte27 <william_fonte@hotmail.com>
2022-03-01 11:28:25 -0300

    Creating the model User

---
+++ app/Models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'checked',
+        'description',
+        'interest',
+        'date_of_birth',
+        'email',
+        'account',
+        'credit_card_type',
+        'credit_card_number',
+        'credit_card_name',
+        'credit_card_expirationDate'
+    ];
+
+}
